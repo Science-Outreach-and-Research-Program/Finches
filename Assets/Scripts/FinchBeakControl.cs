d7e80d9c4ccb56457fbe84d143b74dc33c81ff82
LeoLiu1999 <yliu6@stuy.edu>
2021-04-10 16:54:08 -0400

    Added seed-related scripts

--- Assets/Scripts/FinchBeakControl.cs
+++ Assets/Scripts/FinchBeakControl.cs
@@ -7,6 +7,9 @@ public class FinchBeakControl : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     public Sprite BeakClosedSprite;
     public Sprite BeakOpenSprite;
+    public GameObject seed;
+    public static readonly Vector3 offset = new Vector3(1.5f, 0.08f, -0.1f);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,12 +25,43 @@ public class FinchBeakControl : MonoBehaviour
         {
             Debug.Log("Space key was pressed.");
             spriteRenderer.sprite = BeakClosedSprite;
+
+            
+            GameObject[] seeds = GameObject.FindGameObjectsWithTag("Seed");
+            GameObject closestSeed = null;
+            float closestDistance = 1;
+            foreach (GameObject seed in seeds)
+            {
+                float distance = Vector3.Distance(transform.position - offset, seed.transform.position);
+                if (distance < closestDistance)
+                {
+                    closestSeed = seed;
+                }
+            }
+            this.seed = closestSeed;
+            if (seed)
+            {
+                // seed.GetComponent<SeedControl>().UpdateState(SeedControl.IN_BEAK, 100);
+                seed.GetComponent<SeedControl>().SetFinch(gameObject);
+                Debug.Log("calling SeedControl.UpdateState");
+            }
+            else
+            {
+                Debug.Log("No valid seed detected");
+            }
         }
 
         if (Input.GetKeyUp(KeyCode.Space))
         {
             Debug.Log("Space key was released.");
             spriteRenderer.sprite = BeakOpenSprite;
+
+            if (seed)
+            {
+                // seed.GetComponent<SeedControl>().UpdateState(SeedControl.FALLING, 0);
+                seed.GetComponent<SeedControl>().SetFinch(null);
+                seed = null;
+            }
         }
     }
 }
---
+++ Assets/Scripts/SeedControl.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SeedControl : MonoBehaviour
+{
+    public const int IN_BUSH = 0;
+    public const int IN_BEAK = 1;
+    public const int IN_NEST = 2;
+    public const int FALLING = 3;
+
+
+    public float speed;
+    public int timeUntilFalling = 3600;
+    public int state;
+    private Rigidbody2D rigidbody2D;
+    private GameObject finch;
+    
+    public void SetFinch(GameObject finch)
+    {
+        this.finch = finch;
+        if (finch)
+        {
+            UpdateState(IN_BEAK, 100);
+        }
+        else
+        {
+            UpdateState(FALLING, 0);
+        }
+    }
+
+    public void UpdateState(int state, int timeUntilFalling)
+    {
+        this.state = state;
+        if (state == IN_BUSH)
+        {
+            this.timeUntilFalling = timeUntilFalling;
+            rigidbody2D.gravityScale = 0;
+            rigidbody2D.velocity = Vector2.zero;
+        }
+        if (state == IN_BEAK)
+        {
+            this.timeUntilFalling = timeUntilFalling;
+            rigidbody2D.gravityScale = 0;
+            rigidbody2D.velocity = Vector2.zero;
+        }
+        if (state == IN_NEST)
+        {
+            this.timeUntilFalling = timeUntilFalling;
+            rigidbody2D.gravityScale = 0;
+            rigidbody2D.velocity = Vector2.zero;
+        }
+        if (state == FALLING)
+        {
+            rigidbody2D.gravityScale = 1;
+        }
+        Debug.Log("UPDATE STATE!\nState " + state + ", timeUntilFalling " + timeUntilFalling);
+    }
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 10f;
+        QualitySettings.vSyncCount = 1;
+
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        UpdateState(IN_BUSH, 3600);
+        // rigidbody2D.freezePosition = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == FALLING)
+        {
+            timeUntilFalling--;
+            if (timeUntilFalling < 0)
+            {
+                UpdateState(FALLING, 0);
+            }
+        }
+        
+        if (state == IN_BEAK)
+        {
+            Vector3 finchPos = finch.transform.position;
+            transform.position = finchPos - FinchBeakControl.offset;
+        }
+    }
+}
