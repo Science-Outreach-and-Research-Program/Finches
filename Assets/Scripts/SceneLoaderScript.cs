81c474e29ac4f7efe748b6442e53710b2f77c108
Andrew Qu <aq447@nyu.edu>
2021-04-24 16:26:20 -0400

    add StartScene, add SceneManagement

---
+++ Assets/Scripts/SceneLoaderScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderScript : MonoBehaviour
+{
+    IEnumerator LoadScene(string sceneToLoad, string playerFinch)
+    {
+        string currScene = SceneManager.GetActiveScene().name;
+        SceneManager.UnloadSceneAsync(currScene);
+        SceneManager.LoadScene(sceneToLoad);
+        while (SceneManager.GetActiveScene().name != sceneToLoad)
+        {
+            yield return null;
+        }
+
+        GameObject[] finches = GameObject.FindGameObjectsWithTag("Finch");
+        Debug.Log(finches.Length);
+        foreach(GameObject f in finches)
+        {
+            if (f.name != playerFinch)
+            {
+                f.SetActive(false);
+            }
+        }
+    }
+
+    public void LoadRoundOne(string playerFinch)
+    {
+        StartCoroutine(LoadScene("RoundOne",playerFinch));
+    }
+}
